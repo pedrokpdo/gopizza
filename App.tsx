5ac5a058033962828492bc96a69855aa3305d36a
pedrokpdo <pedromotoehm@gmail.com>
2022-05-16 13:51:32 -0300

    path mapping

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import teste from '@screens/teste'
 
 export default function App() {
   return (
